a68a95f5fa3fdf348916e86eb5fe3b02dcb79353
Valery Buz <Ajven@yandex.ru>
2021-12-04 19:43:29 +0300

    Change code

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int refill  = 1100;
-        int bonus_rub = 1;
+        int refill = 1100;
+        int bonusRub = 1;
 
-        int bonus;
-        if (refill > 1000);{
-            bonus = refill / 100 * bonus_rub;
+        int bonus = 0;
+        if (refill > 1000) {
+            bonus = refill / 100 * bonusRub;
         }
         System.out.println("Bonus = " + bonus + " rub");
         balance = bonus + refill + balance;
         System.out.println("Balance = " + balance + " rub");
     }
-    }
+}
